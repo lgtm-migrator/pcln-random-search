bdb7eb1d1e97cbbf9aa7829e3fed3db1da8238d5
Craig Palermo <craig.palermo@opmbx.org>
2019-03-27 08:05:54 -0400

    Add B environments

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,14 @@ export const envs = {
   'dev-a': {
     urlRoot: 'www.a1.dev.priceline.com',
   },
+  'dev-b': {
+    urlRoot: 'www.b1.dev.priceline.com',
+  },
   qaa: {
     urlRoot: 'www.qaa.priceline.com',
+  },
+  qab: {
+    urlRoot: 'www.qab.priceline.com',
   },
   'pre-prod': {
     urlRoot: 'www.a309.corp.priceline.com',
